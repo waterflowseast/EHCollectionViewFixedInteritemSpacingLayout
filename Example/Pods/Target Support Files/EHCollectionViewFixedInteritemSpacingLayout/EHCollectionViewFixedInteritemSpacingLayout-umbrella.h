e0b964228926d25fee671eefe976957a2c5c6ff8
Eric Huang <WaterFlowsEast@gmail.com>
2017-01-10 18:48:50 +0800

    pod install for Example

--- Example/Pods/Target Support Files/EHCollectionViewFixedInteritemSpacingLayout/EHCollectionViewFixedInteritemSpacingLayout-umbrella.h
+++ Example/Pods/Target Support Files/EHCollectionViewFixedInteritemSpacingLayout/EHCollectionViewFixedInteritemSpacingLayout-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "EHCollectionViewFixedInteritemSpacingLayout.h"
 
 FOUNDATION_EXPORT double EHCollectionViewFixedInteritemSpacingLayoutVersionNumber;
 FOUNDATION_EXPORT const unsigned char EHCollectionViewFixedInteritemSpacingLayoutVersionString[];
